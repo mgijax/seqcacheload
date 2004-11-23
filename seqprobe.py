3f87681d284366eeb0ee3c7cd950fcaba6d7befe
Lori Corbani <Lori.Corbani@jax.org>
2004-11-23 14:26:39 +0000

    TR 6118

--- seqprobe.py
+++ seqprobe.py
@@ -16,6 +16,9 @@
 #
 # History
 #
+# 11/23/2004	lec
+#	- added createExcluded() for TR 6118 (GXD Gray data load)
+#
 # 10/23/2003	lec
 #	- new (TR 3404, JSAM)
 #
@@ -33,51 +36,78 @@ table = os.environ['TABLE']
 userKey = 0
 loaddate = loadlib.loaddate
 
-def createBCP():
+def createExcluded():
 
-	print 'Creating %s.bcp...%s' % (table, mgi_utils.date())
+    excludeNote = 'The source of the material used to create this cDNA probe was different than that used to create the GenBank sequence record.'
+
+    print 'excluded begin...%s' % (mgi_utils.date())
+    cmds = []
+    cmds.append('select _Probe_key into #excluded from PRB_Notes ' + \
+	'where note like "The source of the material used to create this cDNA probe was different%"')
+    cmds.append('create index idx1 on #excluded(_Probe_key)')
+    db.sql(cmds, None)
+    print 'excluded end...%s' % (mgi_utils.date())
+
+def createBCP():
 
 	outBCP = open('%s.bcp' % (table), 'w')
 
+	print 'sequences1 begin...%s' % (mgi_utils.date())
 	cmds = []
-	cmds.append('select sequenceKey = s._Object_key, ' + \
-		'probeKey = p._Object_key, refsKey = ar._Refs_key, ' + \
-		'mdate = convert(char(10), p.modification_date, 101) ' + \
-		'into #sequences ' + \
-		'from ACC_Accession s, ACC_Accession p, ACC_AccessionReference ar ' + \
+	cmds.append('select sequenceKey = s._Object_key, probeKey = p._Object_key, p._Accession_key ' + \
+		'into #sequences1 ' + \
+		'from ACC_Accession s, ACC_Accession p ' + \
 		'where s._MGIType_key = 19 ' + \
 		'and s.accID = p.accID ' + \
 		'and p._MGIType_key = 3 ' + \
-		'and s._LogicalDB_key = p._LogicalDB_key ' + \
-		'and p._Accession_key = ar._Accession_key')
+		'and s._LogicalDB_key = p._LogicalDB_key ')
+	cmds.append('create index idx1 on #sequences1 (sequenceKey)')
+	cmds.append('create index idx2 on #sequences1 (probeKey)')
+	cmds.append('create index idx3 on #sequences1 (_Accession_key)')
+	db.sql(cmds, None)
+	print 'sequences1 end...%s' % (mgi_utils.date())
 
-	cmds.append('create nonclustered index idx_seq on #sequences (sequenceKey)')
-	cmds.append('create nonclustered index idx_mrk on #sequences (probeKey)')
-	cmds.append('create nonclustered index idx_ref on #sequences (refsKey)')
-	cmds.append('create nonclustered index idx_mdt on #sequences (mdate)')
+	print 'deletion begin...%s' % (mgi_utils.date())
+	db.sql('delete #sequences1 from #sequences1 s, #excluded e where s.probeKey = e._Probe_key', None)
+	print 'deletion end...%s' % (mgi_utils.date())
 
-	cmds.append('select distinct sequenceKey, probeKey, refsKey, mdate = max(mdate) from #sequences ' + \
-		'group by sequenceKey, probeKey, refsKey')
+	print 'sequences2 begin...%s' % (mgi_utils.date())
+	cmds = []
+	cmds.append('select s.sequenceKey, s.probeKey, refsKey = ar._Refs_key, ' + \
+		'mdate = convert(char(10), ar.modification_date, 101) ' + \
+		'into #sequences2 ' + \
+		'from #sequences1 s, ACC_AccessionReference ar ' + \
+		'where s._Accession_key = ar._Accession_key')
+	cmds.append('create index idx1 on #sequences2 (sequenceKey)')
+	cmds.append('create index idx2 on #sequences2 (probeKey)')
+	cmds.append('create index idx3 on #sequences2 (refsKey)')
+	cmds.append('create index idx4 on #sequences2 (mdate)')
+	db.sql(cmds, None)
+	print 'sequences2 end...%s' % (mgi_utils.date())
 
-	results = db.sql(cmds, 'auto')
+	print 'final begin...%s' % (mgi_utils.date())
+	results = db.sql('select distinct sequenceKey, probeKey, refsKey, mdate = max(mdate) from #sequences2 ' + \
+		'group by sequenceKey, probeKey, refsKey', 'auto')
+	print 'final end...%s' % (mgi_utils.date())
 
-	for r in results[-1]:
-
+	for r in results:
 		outBCP.write(mgi_utils.prvalue(r['sequenceKey']) + DL + \
 		       	mgi_utils.prvalue(r['probeKey']) + DL + \
 		       	mgi_utils.prvalue(r['refsKey']) + DL + \
 			r['mdate'] + DL + \
 			str(userKey) + DL + str(userKey) + DL + \
 			loaddate + DL + loaddate + NL)
-
 	outBCP.close()
 
 #
 # Main Routine
 #
 
+db.useOneConnection(1)
 userKey = loadlib.verifyUser(os.environ['DBUSER'], 1, None)
 print '%s' % mgi_utils.date()
+createExcluded()
 createBCP()
+db.useOneConnection(0)
 print '%s' % mgi_utils.date()
 
