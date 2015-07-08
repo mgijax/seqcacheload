90db3ed7e0d7435cf65b4dd3998e1f831f408ea5
MGI Admin <mgiadmin@jax.org>
2015-07-08 15:39:29 +0000

    TR12058/vega 61

--- seqmarker.py
+++ seqmarker.py
@@ -1152,7 +1152,7 @@ def generateBiotypeLookups():
     vegaEquiv7 = string.lower(os.environ['VEGA_EQUIV7'])
     vegaEquiv8 = string.lower(os.environ['VEGA_EQUIV8'])
 
-    vegaEquiv = '%s,%s,%s,%s,%s,%s,%s' % (vegaEquiv1, vegaEquiv2, vegaEquiv3, vegaEquiv4, vegaEquiv5, vegaEquiv6, vegaEquiv7, vegaEquiv8)
+    vegaEquiv = '%s,%s,%s,%s,%s,%s,%s,%s' % (vegaEquiv1, vegaEquiv2, vegaEquiv3, vegaEquiv4, vegaEquiv5, vegaEquiv6, vegaEquiv7, vegaEquiv8)
     mappingList = string.split(vegaEquiv, ',')
     for m in mappingList:
 	rawList = string.split(m, ':')
